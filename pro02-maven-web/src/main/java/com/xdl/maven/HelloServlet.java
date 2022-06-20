966bd472bd6e51cea36e1ddf87643004b8dd8f32
xdl <13333759329@163.com>
2022-06-21 00:26:30 +0800

    依赖范围 compile test provided

--- pro02-maven-web/src/main/java/com/xdl/maven/HelloServlet.java
+++ pro02-maven-web/src/main/java/com/xdl/maven/HelloServlet.java
@@ -5,7 +5,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 import java.io.IOException;
+// 测试compile依赖范围对main目录有效
+import com.xdl.maven.Calculator;
+// 测试test依赖范围对main目录无效
+import org.junit.Test;
 
+/**
+ * 测试方式：
+ * 通过import语句将要测试的类引入到当前类，引入后：
+ * 编译通过：可以使用，这个范围的依赖对当前类有效
+ * 编译失败：不能使用，这个范围的依赖对当前类无效
+ * main 目录下的java类：HelloServlet
+ * compile 范围引入的依赖：pro01-maven-java
+ * pro01-maven-java中的类： Calculator
+ * -------------------------------------
+ * 测试test依赖范围
+ */
 public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws
             ServletException, IOException {
--- pro02-maven-web/src/test/java/com/xdl/maven/CalculatorTest.java
+++ pro02-maven-web/src/test/java/com/xdl/maven/CalculatorTest.java
@@ -5,7 +5,18 @@ import com.xdl.maven.Calculator;
 //静态导入的效果是将Assert类中的静态资源导入当前类
 //这样以来，在当前类中就可以直接使用Asswert类中的静态资源，不需要写类名
 import static org.junit.Assert.*;
+// 测试compile依赖范围对main目录有效
+import com.xdl.maven.Calculator;
 
+/**
+ * 测试方式：
+ * 通过import语句将要测试的类引入到当前类，引入后：
+ * 编译通过：可以使用，这个范围的依赖对当前类有效
+ * 编译失败：不能使用，这个范围的依赖对当前类无效
+ * main 目录下的java类：CalculatorTest
+ * compile 范围引入的依赖：pro01-maven-java
+ * pro01-maven-java中的类： Calculator
+ */
 public class CalculatorTest {
     @Test
     public void testSum() {
